93a0d35086d210c8331394837957be9f9e207efd
Imbdang <dtc225200211@ictu.edu.vn>
2025-06-02 06:22:15 +0700

    Keep doing

--- request_handle/lms.py
+++ request_handle/lms.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 from .crc32 import return_signature
+import requests
 import os
 
 class LMS:
     def __init__(self, user):
         load_dotenv()
+        self.class_plan_url = os.getenv("class-plan-url")
         self.user = user
         self.courses = user.get_courses()
         self.x_app_id = os.getenv("x-app-id")
@@ -19,10 +21,9 @@ class LMS:
         self.user_agent = os.getenv("user-agent")
         self.head = {
             "Accept": "application/json, text/plain, */*",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
+            # "Accept-Encoding": "gzip, deflate, br, zstd",
             "Accept-Language": "en-US,en;q=0.9",
             "Authorization": "Bearer " + self.user.access_token, 
-            "Content-Type": "application/json",
             "Host": self.host,
             "Origin": self.domain,
             "Referer": self.domain + "/",
@@ -52,6 +53,7 @@ class LMS:
         isAble = False
         yourCourse = None
         while True:
+            print("\nNhap 0 de thoat khoi chuong trinh.")
             self.view_courses()
             choice = int(input("Chon ID mon hoc: "))
             for i in self.courses:
@@ -61,13 +63,53 @@ class LMS:
                     isAble = True
                     input("Nhan Enter de tiep tuc...")
                     break
-            else:
-                print("ID mon hoc khong hop le.")
-                input("Nhan Enter de tiep tuc...")
+                elif choice == 0:
+                    break
+                else:
+                    print("ID mon hoc khong hop le.")
+                    input("\nNhan Enter de tiep tuc...")
             
             if isAble:
-                print("Bat dau hoc...")
-                for lesson in yourCourse:
-                    print(lesson)
-                input("Nhan Enter de tiep tuc...")
+                self.get_class_plan(yourCourse)
+
+    def get_class_plan(self, course):
+        self.head["X-Request-Signature"] = return_signature(
+            method="GET",
+            body={}
+        )
+        response = requests.get(
+            url=self.class_plan_url,
+            headers=self.head,
+            params = {
+                "limit": 1000,
+                "paged": 1,
+                "orderby": "week",
+                "order": "ASC",
+                "select": "id,class_id,course_id,course_plan_activity_id,week,title,date_start_of_week,date_end_of_week,teaching_day",
+                "condition[0][key]": "class_id",
+                "condition[0][value]": course.get("id"),
+                "condition[0][compare]": "=",
+                "condition[1][key]": "week",
+                "condition[1][value]": "1000",
+                "condition[1][compare]": "<>"
+            }
+        )
+        if response.status_code == 200:
+            print(response.text)
+            response_data = response.json()
+            data = response_data.get("data")
+            while True:
+                print("Nhap 0 de exit")
+                print(f"So luong tuan hoc la {response_data.get('count')}, vui long chon tuan hoc ban muon: ")
+                week = int(input("Nhap tuan hoc: "))
+                if week == 0:
+                    print("Exiting...")
+                    break
+                elif 1 <= week <= response_data.get("count"):
+                    print(data[week-1])
+                else:
+                    print("Tuan hoc khong hop le, vui long nhap lai.")
+
+        else:
+            print(f"Error fetching class plan: {response.status_code} - {response.text}")
      
